99d377affe4caddb01cb0543dd4c1e6ca685cf34
Arrak Yambupah <arrak.ya@outlook.com>
2021-08-14 11:50:02 +0700

    Docuements Navigate up and down

--- src/app/docs/docs.component.ts
+++ src/app/docs/docs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DocsModel } from './docs.model';
 import { DocsService } from './docs.service';
+import { faHome, faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-docs',
@@ -9,13 +10,55 @@ import { DocsService } from './docs.service';
 })
 export class DocsComponent implements OnInit {
 
-  docsCollection: DocsModel[];
+  FaHome = faHome;
+  FaArrowUp = faArrowUp;
+
+  DocsCollection: DocsModel[];
+
+  PathCollection : string[];
+
+  PathString = "";
+
+  PathCurrent = "";
+
+  IsShowGoBackItem = false;
 
   constructor(docsService: DocsService) { 
-    this.docsCollection = docsService.getDocuments();
+    this.DocsCollection = docsService.getDocuments();
+
+    this.PathCollection = [];       
   }
 
   ngOnInit(): void {
   }
 
+  concatPathString(){
+    this.PathString = "";
+    this.PathCurrent = "";
+    if(this.PathCollection.length > 1){
+      for(let i = 0; i < (this.PathCollection.length - 1); i++){    
+        this.PathString += this.PathCollection[i];
+        this.PathString += " / ";
+      }    
+    }
+
+    this.PathCurrent = this.PathCollection[this.PathCollection.length - 1];
+  }
+
+  onClickExploreItem(document: DocsModel){
+    this.PathCollection[this.PathCollection.length] = document.Name;
+    this.concatPathString();
+    this.setShowGoBackItem();    
+  }
+
+  setShowGoBackItem(){
+    this.IsShowGoBackItem = this.PathCollection.length > 0;
+  }
+
+  onClickGoBackItem(){    
+    this.PathCollection.pop();
+    this.concatPathString();
+    this.setShowGoBackItem();
+    console.log(this.PathCollection);
+  }
 }
--- src/app/docs/docs.service.ts
+++ src/app/docs/docs.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { DocsModel } from './docs.model';
-import { faFolder, faFile } from '@fortawesome/free-solid-svg-icons';
+import { faFileWord, faFileInvoice } from '@fortawesome/free-solid-svg-icons';
 
 @Injectable({
   providedIn: 'root'
@@ -11,28 +11,28 @@ export class DocsService {
 
   getDocuments(){
     let docs = [
-      new DocsModel(1, "Folder 1", faFolder, "explorer-item folder"),
-      new DocsModel(2, "Folder 2", faFolder, "explorer-item folder"),
-      new DocsModel(3, "Folder 3", faFolder, "explorer-item folder"),
-      new DocsModel(4, "Folder 4", faFolder, "explorer-item folder"),
-      new DocsModel(5, "Folder 5", faFolder, "explorer-item folder"),
-      new DocsModel(6, "Folder 6", faFolder, "explorer-item folder"),
-      new DocsModel(7, "Folder 7", faFolder, "explorer-item folder"),
-      new DocsModel(8, "Folder 8", faFolder, "explorer-item folder"),
-      new DocsModel(9, "Folder 9", faFolder, "explorer-item folder"),
-      new DocsModel(10, "Folder 10", faFolder, "explorer-item folder"),
-      new DocsModel(11, "Folder 11", faFolder, "explorer-item folder"),
-      new DocsModel(12, "Folder 12", faFolder, "explorer-item folder"),
-      new DocsModel(13, "File 1", faFile, "explorer-item file"),
-      new DocsModel(14, "File 2", faFile, "explorer-item file"),
-      new DocsModel(15, "File 3", faFile, "explorer-item file"),
-      new DocsModel(16, "File 4", faFile, "explorer-item file"),
-      new DocsModel(17, "File 5", faFile, "explorer-item file"),
-      new DocsModel(18, "File 6", faFile, "explorer-item file"),
-      new DocsModel(19, "File 7", faFile, "explorer-item file"),
-      new DocsModel(20, "File 8", faFile, "explorer-item file"),
-      new DocsModel(21, "File 9", faFile, "explorer-item file"),
-      new DocsModel(22, "File 10", faFile, "explorer-item file")
+      new DocsModel(1, "Folder with very very long name", faFileWord, "explorer-item folder"),
+      new DocsModel(2, "Folder 2", faFileWord, "explorer-item folder"),
+      new DocsModel(3, "Folder 3", faFileWord, "explorer-item folder"),
+      new DocsModel(4, "Folder 4", faFileWord, "explorer-item folder"),
+      new DocsModel(5, "Folder 5", faFileWord, "explorer-item folder"),
+      new DocsModel(6, "Folder 6", faFileWord, "explorer-item folder"),
+      new DocsModel(7, "Folder 7", faFileWord, "explorer-item folder"),
+      new DocsModel(8, "Folder 8", faFileWord, "explorer-item folder"),
+      new DocsModel(9, "Folder 9", faFileWord, "explorer-item folder"),
+      new DocsModel(10, "Folder 10", faFileWord, "explorer-item folder"),
+      new DocsModel(11, "Folder 11", faFileWord, "explorer-item folder"),
+      new DocsModel(12, "Folder 12", faFileWord, "explorer-item folder"),
+      new DocsModel(13, "File with very long long name", faFileInvoice, "explorer-item file"),
+      new DocsModel(14, "File 2", faFileInvoice, "explorer-item file"),
+      new DocsModel(15, "File 3", faFileInvoice, "explorer-item file"),
+      new DocsModel(16, "File 4", faFileInvoice, "explorer-item file"),
+      new DocsModel(17, "File 5", faFileInvoice, "explorer-item file"),
+      new DocsModel(18, "File 6", faFileInvoice, "explorer-item file"),
+      new DocsModel(19, "File 7", faFileInvoice, "explorer-item file"),
+      new DocsModel(20, "File 8", faFileInvoice, "explorer-item file"),
+      new DocsModel(21, "File 9", faFileInvoice, "explorer-item file"),
+      new DocsModel(22, "File 10", faFileInvoice, "explorer-item file")
     ] 
 
     return docs;
